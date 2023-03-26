a25e17ccb539772eaa93624173561487cbb842d1
samaneh <samanehb23@gmail.com>
2023-03-26 18:29:47 +0430

    Header component updated

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import Home from '../pages/Home';
 import Shop from '../pages/Shop';
 import Cart from '../pages/Cart';
@@ -9,6 +9,7 @@ import Signup from '../pages/Signup';
 
 function Routers() {
   return ( <Routes>
+    <Route path='/' element={<Navigate to='home'/>}/>
     <Route path='home' element={<Home/>}/>
     <Route path='shop' element={<Shop/>}/>
     <Route path='cart' element={<Cart/>}/>
