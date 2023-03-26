3f6e0976acc036b619528727a5a6125645fc1ae2
samaneh <samanehb23@gmail.com>
2023-03-26 15:12:47 +0430

    All pages added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, ipsum eligendi similique in fugiat laboriosam eaque iusto velit rerum. Molestiae, modi minus? Blanditiis dignissimos ipsam est reiciendis delectus natus possimus!</p>
+    </div>
+  )
+}
+
+export default App
